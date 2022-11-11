dc7b8ca137ea81340d6fca28c13aa55d8ed1d732
smahatme9 <111636371+smahatme9@users.noreply.github.com>
2022-11-11 19:58:17 +0530

    comment added

--- scprit.js
+++ scprit.js
@@ -2,19 +2,20 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const ampm = document.getElementById("am-pm");
-
+  // hare function given to get time 
 function updateTime() {
   let hrs = new Date().getHours();
   let mins = new Date().getMinutes();
   let sec = new Date().getSeconds();
   
+  // AM and PM function 
   let am_pm = "AM";
   if(hrs > 12){
     hrs -= 12;
     am_pm = "PM";
   }
   
-  hrs = (hrs<10) ? "0" + hrs : hrs;
+  hrs = (hrs<10) ? " 0 "  + hrs : hrs;
   mins = (mins<10) ? "0" + mins : mins;
   sec = (sec<10) ? "0" + sec : sec;
   
@@ -27,4 +28,5 @@ function updateTime() {
     updateTime();
   }, 1000)
 }
+// for update time 
 updateTime();
